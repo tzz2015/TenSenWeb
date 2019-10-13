2212a7790a5c54017bda3cc4d5304d1926f60e03
tzz2015 <1374329135@qq.com>
2019-10-13 23:45:41 +0800

    脚本

--- wx-mini-start.sh
+++ wx-mini-start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /home/python-project/wx-mini
+sudo /etc/init.d/sunny restart
 sudo python manage.py makemigrations
 sudo python manage.py migrate
 sudo kill -9 $(lsof -i:8000 |awk '{print $2}' | tail -n 2)
