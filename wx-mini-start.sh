ed91fc9b1c5232cf79f3810ef1d328ae66128ecf
tzz2015 <1374329135@qq.com>
2019-10-16 19:48:01 +0800

    新增转发脚本

--- wx-mini-start.sh
+++ wx-mini-start.sh
@@ -5,5 +5,6 @@ sudo setsid ./sunny clientid 462d2b0c471d87f1 &
 sudo python3 manage.py makemigrations
 sudo python3 manage.py migrate
 sudo kill -9 $(lsof -i:8000 |awk '{print $2}' | tail -n 2)
-nohup sudo python3 manage.py runserver 0.0.0.0:8000 &
+#nohup sudo python3 manage.py runserver 0.0.0.0:8000 &
+nohup sudo python3 manage.py runsslserver 0.0.0.0:8000 --certificate 2949114_lyf.test.link-nemo.com.pem --key 2949114_lyf.test.link-nemo.com.key &
 
