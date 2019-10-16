3576914824b511df380e2335b854c876d450bd35
tzz2015 <1374329135@qq.com>
2019-10-16 12:03:49 +0800

    新增转发脚本

--- wx-mini-start.sh
+++ wx-mini-start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd /home/python-project/wx-mini
-sudo /etc/init.d/sunny restart
-sudo python manage.py makemigrations
-sudo python manage.py migrate
+sudo setsid ./sunny clientid 462d2b0c471d87f1 &
+#sudo /etc/init.d/sunny restart
+sudo python3 manage.py makemigrations
+sudo python3 manage.py migrate
 sudo kill -9 $(lsof -i:8000 |awk '{print $2}' | tail -n 2)
-nohup python manage.py runserver 0.0.0.0:8000 &
+nohup sudo python3 manage.py runserver 0.0.0.0:8000 &
 
